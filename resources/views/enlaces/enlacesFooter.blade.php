94192545b0cfede6de0900edee7f328375a1dbe0
ohhgrizzly <cataamarande@gmail.com>
2024-05-30 16:15:24 +0200

    Update enlacesFooter

--- resources/views/enlaces/enlacesFooter.blade.php
+++ resources/views/enlaces/enlacesFooter.blade.php
@@ -1,13 +1,13 @@
-<script src="{{ asset('assets/vendor/aos/aos.js') }}"></script>
-<script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
-<script src="{{ asset('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
-<script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
-<script src="{{ asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+<script src="{{ secure_asset('assets/vendor/aos/aos.js') }}"></script>
+<script src="{{ secure_asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ secure_asset('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+<script src="{{ secure_asset('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+<script src="{{ secure_asset('assets/vendor/php-email-form/validate.js') }}"></script>
+<script src="{{ secure_asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
 
 <!-- Template Main JS File -->
-<script src="{{ asset('assets/js/main.js') }}"></script>
+<script src="{{ secure_asset('assets/js/main.js') }}"></script>
 
-<script src="{{ asset('assets/js/jquery-3.2.1.min.js') }}"></script>
-<script src="{{ asset('assets/js/popper.min.js') }}"></script>
-<script src="{{ asset('assets/js/script2.js') }}"></script>
+<script src="{{ secure_asset('assets/js/jquery-3.2.1.min.js') }}"></script>
+<script src="{{ secure_asset('assets/js/popper.min.js') }}"></script>
+<script src="{{ secure_asset('assets/js/script2.js') }}"></script>
