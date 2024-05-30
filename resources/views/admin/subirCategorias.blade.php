6eddcbcfc39a6804239bd8a79954965a23e436ad
ohhgrizzly <cataamarande@gmail.com>
2024-05-30 19:43:59 +0200

    Update subirCategorias

--- resources/views/admin/subirCategorias.blade.php
+++ resources/views/admin/subirCategorias.blade.php
@@ -118,6 +118,100 @@
                 document.getElementById('eliminarSeleccionados').disabled = checkedCount === 0;
             });
         });
+
+        document.addEventListener('DOMContentLoaded', function () {
+  var categoriaSelect = document.getElementById('categoria');
+  if (categoriaSelect) {
+    categoriaSelect.addEventListener('change', function () {
+      if (this.value === 'add_new') {
+        var formato = prompt('Introduce el nuevo formato de categoría:');
+        if (formato) {
+          if (!esAdmin) {
+            alert('Acceso denegado');
+            return;
+        }
+          // Verificar la existencia de la categoría
+          fetch('/admin/verificar-categoria', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'X-CSRF-TOKEN': csrfToken
+            },
+            body: JSON.stringify({ formato: formato })
+          })
+          .then(response => response.json())
+          .then(data => {
+            if (data.exists) {
+              alert('Error: Esta categoría ya existe.');
+            } else {
+              // Si la categoría no existe, enviar el formulario para crearla
+              fetch(crearCategoriaUrl, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'X-CSRF-TOKEN': csrfToken
+                },
+                body: JSON.stringify({ formato: formato })
+              })
+              .then(response => response.json())
+              .then(data => {
+                if (data.success) {
+                  alert(data.message);
+                  location.reload(); // Recargar la página para actualizar el desplegable
+                } else {
+                  alert('Hubo un error al crear la categoría.');
+                }
+              })
+              .catch(error => {
+                console.error('Error:', error);
+                alert('Hubo un error al crear la categoría.');
+              });
+            }
+          })
+          .catch(error => {
+            console.error('Error:', error);
+            alert('Hubo un error al verificar la categoría.');
+          });
+        }
+      }
+    });
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('subcategoriaForm').addEventListener('submit', function (event) {
+      event.preventDefault();
+      
+      fetch('/admin/crearSubcategoria', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'X-CSRF-TOKEN': csrfToken
+          },
+          body: JSON.stringify({
+              categoria: document.getElementById('categoria').value,
+              tema: document.getElementById('tema').value,
+              material: document.getElementById('material').value,
+              tipo: document.getElementById('tipo').value,
+              epoca: document.getElementById('epoca').value,
+          })
+      })
+      .then(response => response.json())
+      .then(data => {
+          if (data.error) {
+              alert(data.error);
+          } else if (data.success) {
+              alert(data.success);
+              location.reload();
+          }
+      })
+      .catch(error => {
+          console.error('Error:', error);
+          alert('Hubo un error al enviar el formulario.');
+      });
+  });
+  
+});
     </script>
 
     @include('footer')
