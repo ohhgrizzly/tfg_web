ff3a53a807835d611af16df4bea10605c199cae5
ohhgrizzly <cataamarande@gmail.com>
2024-05-30 15:51:52 +0200

    Update obras migracion

--- database/migrations/2024_05_08_064028_create_obras_table.php
+++ database/migrations/2024_05_08_064028_create_obras_table.php
@@ -7,19 +7,18 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     public function up(): void
-{
-    Schema::create('obras', function (Blueprint $table) {
-        $table->id();
-        $table->timestamps();
-        $table->string('titulo')->unique();
-        $table->text('descripcion');
-        $table->string('imagen');
-        $table->foreignId('id_usuario')->constrained('usuarios')->onDelete('cascade');
-        $table->foreignId('subcategoria_id')->constrained('subcategorias')->onDelete('cascade');
-        $table->string('autor');
-    });
-}
-
+    {
+        Schema::create('obras', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('titulo')->unique();
+            $table->text('descripcion');
+            $table->string('imagen');
+            $table->foreignId('id_usuario')->constrained('usuarios')->onDelete('cascade');
+            $table->foreignId('subcategoria_id')->constrained('subcategorias')->onDelete('cascade');
+            $table->string('autor');
+        });
+    }
 
     public function down(): void
     {
