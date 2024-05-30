c1b5b752fcecf7b521475301fd2bec7d6c60716f
ohhgrizzly <cataamarande@gmail.com>
2024-05-30 18:19:10 +0200

    Update header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -15,7 +15,7 @@
       <div class="container d-flex align-items-center justify-content-between">
   
         <div class="logo">
-          <a href="/"><img src="{{ asset('assets/img/logo.png') }}" alt="" class="img-fluid"></a>
+          <a href="/"><img src="{{ secure_asset('assets/img/logo.png') }}" alt="" class="img-fluid"></a>
         </div>
         
         <!-- .navbar -->
