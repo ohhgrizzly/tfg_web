919dcf7866c4f39fcd1cced33c7f34a1897548cf
ohhgrizzly <cataamarande@gmail.com>
2024-05-31 10:42:12 +0200

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -39,7 +39,7 @@
                     <div class="col-lg-3 col-md-6 portfolio-item filter-{{ $obra->subcategoria->categoria->id }}">
                         <div class="portfolio-wrap">
                             <a href="{{ route('obras.mostrar', ['id' => $obra->id]) }}">
-                                <img src="{{ secure_asset('assets/img/imagenesObras/' . $obra->imagen) }}" class="img-fluid" alt="">
+                                <img src="{{ secure_asset('storage/assets/img/imagenesObras/' . $obra->imagen) }}" class="img-fluid" alt="">
                                 <div class="portfolio-info">
                                     <h4>{{ $obra->titulo }}</h4>
                                     <p>Autor: {{ $obra->autor }}</p>
@@ -74,7 +74,7 @@
                 <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
                   <div class="member" data-aos="zoom-in">
                     <div class="member-img">
-                      <img src="{{ secure_asset('assets/img/team/escultura.png') }}" class="img-fluid" alt="">
+                      <img src="{{ secure_asset('storage/assets/img/team/escultura.png') }}" class="img-fluid" alt="">
                     </div>
                     <div class="member-info">
                       <h3>Compartir Obras</h3>
@@ -87,7 +87,7 @@
                 <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
                   <div class="member" data-aos="zoom-in" data-aos-delay="100">
                     <div class="member-img">
-                      <img src="{{ secure_asset('assets/img/team/vangogh.png') }}" class="img-fluid" alt="">
+                      <img src="{{ secure_asset('storage/assets/img/team/vangogh.png') }}" class="img-fluid" alt="">
                     </div>
                     <div class="member-info">
                       <h3>Descubrir Arte</h3>
@@ -100,7 +100,7 @@
                 <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
                   <div class="member" data-aos="zoom-in" data-aos-delay="200">
                     <div class="member-img">
-                      <img src="{{ secure_asset('assets/img/team/comunidad.png') }}" class="img-fluid" alt="">
+                      <img src="{{ secure_asset('storage/assets/img/team/comunidad.png') }}" class="img-fluid" alt="">
                     </div>
                     <div class="member-info">
                       <h3>Interactuar</h3>
